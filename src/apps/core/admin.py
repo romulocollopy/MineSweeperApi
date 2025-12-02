59c6d81b1a3b7934b3cbb8c8f3d542ffd7136ab2
Romulo Collopy <rcollopy@simplefractal.com>
2025-12-01 23:44:08 -0500

    display board on admin

--- src/apps/core/admin.py
+++ src/apps/core/admin.py
@@ -1,6 +1,7 @@
 import json
 
 from django.contrib import admin
+from django.utils.html import SafeString
 
 from src.apps.core.models import Game
 
@@ -8,7 +9,9 @@ from src.apps.core.models import Game
 @admin.register(Game)
 class GameAdmin(admin.ModelAdmin):
     list_display = ["id", "slug", "game_over"]
+    readonly_fields = ["formatted_board"]
 
     @admin.display()
     def formatted_board(self, obj):
-        return json.dumps(obj.board, indent=2)
+        board = json.dumps(obj.board, indent=2)
+        return SafeString(f"<pre>{board}</pre>")
